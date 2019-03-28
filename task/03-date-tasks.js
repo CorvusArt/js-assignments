b5cdd6d07cbf24595a95a1fab13bad87d3733b3d
CorvusArt <44847729+CorvusArt@users.noreply.github.com>
2019-03-28 20:20:48 +0300

    lint fix

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -76,7 +76,7 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-  let dif = new Date(endDate.valueOf() - startDate.valueOf());
+  const dif = new Date(endDate.valueOf() - startDate.valueOf());
   return dif.toISOString().slice(-13, -1);
 }
 
