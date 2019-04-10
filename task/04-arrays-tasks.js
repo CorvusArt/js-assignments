5dbfe3bf54bb0c609452583d2097ce78d41db77a
CorvusArt <44847729+CorvusArt@users.noreply.github.com>
2019-04-10 22:12:03 +0300

    arr-14-complete

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -236,7 +236,7 @@ function toArrayOfSquares(arr) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
 function getMovingSum(arr) {
-  throw new Error('Not implemented');
+  return arr.reduce((acc, val, i) => [...acc, val + (acc[i-1] || 0)], []);
 }
 
 /**
